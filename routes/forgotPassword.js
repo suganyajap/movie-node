0dff632aaa6f48a4ce6d8cf13d0c5b80e385cead
SUNDARAVADIVU R <suganyajap@gmail.com>
2022-02-14 14:36:12 +0530

    client url changed

--- routes/forgotPassword.js
+++ routes/forgotPassword.js
@@ -4,7 +4,7 @@ import sgMail from "@sendgrid/mail"
 import { client } from "../index.js"
 
 const router = express.Router();
-const CLIENT_URL = "https://inspiring-ramanujan-6d5121.netlify.app"
+const CLIENT_URL = "http://localhost:3000"
 
 
 router.route('/').put(async (req, res) => {
